2be156947bb2d8f3946ae2bb47170aaa3b1e7ae7
pchamikara <IT21237386@my.sliit.lk>
2025-02-20 03:02:17 +0530

    move data to env

--- backend/controllers/Email.js
+++ backend/controllers/Email.js
@@ -4,16 +4,16 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   service: "gmail", // Use Gmail or any other service
   auth: {
-    user: "kavindasithum127@gmail.com", // Replace with your email
-    pass: "vctv xlau neun iere", // Replace with your email password or app-specific password
+    user: process.env.USER, // Replace with your email
+    pass: process.env.PASSWORD, // Replace with your email password or app-specific password
   },
 });
 
 // Function to send email
 const sendEmail = (name, experience, email, mobile, resumePath, callback) => {
   const mailOptions = {
-    from: "kavindasithum127@gmail.com", // Sender email
-    to: "kwpchamikara99@gmail.com", // Recipient email
+    from: process.env.FROM, // Sender email
+    to: process.env.TO, // Recipient email
     subject: "New Job Application",
     html: `
       <h1>New Job Application</h1>
